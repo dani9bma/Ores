f47bbdf1346052b41d9b991888d2b719ed7d7b75
Daniel Assunção <daniel.assuncao9@gmail.com>
2019-12-06 11:46:31 +0000

    Init commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <SDL.h>
+
+int main(int argc, char *argv[])
+{
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+	{
+		std::cout << "SDL could not be initialized -> " << SDL_GetError() << std::endl;
+	}
+
+	return EXIT_SUCCESS;
+}
